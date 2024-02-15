47827fdaa1e4274b27e9cb225123657595dd13cc
Saikiran Modalavalasa <138604057+saikiran-modalavalasa@users.noreply.github.com>
2024-02-15 12:14:58 +0530

    Contact-us Page

---
+++ Contact-Us/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import ContactForm from './components/ContactForm/ContactForm'
+import ContactHeader from './components/ContactHeader/ContactHeader'
+import Navigation from './components/Navigation/Navigation'
+
+
+
+export default function App() {
+  return (
+    <div>
+      <Navigation />
+      <main className='main_container'>
+        <ContactHeader />
+        <ContactForm />
+      </main>
+    </div>
+  )
+}
