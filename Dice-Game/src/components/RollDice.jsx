6ca6fd394fd7a8c217b13f9591cd96cdd22884ca
saikiran-modalavalasa <modalavalasasaikiran@gmail.com>
2024-02-20 00:08:50 +0530

    Some changes in roll dice

--- Dice-Game/src/components/RollDice.jsx
+++ Dice-Game/src/components/RollDice.jsx
@@ -6,12 +6,19 @@ const RollDice = () => {
   const [currentDice, setCurrentDice] = useState();
 
   const generateRandomNumber = (min, max) =>{
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  const onRollDice = () => {
+    const randomNumber = generateRandomNumber (1, 7)
+
+    setCurrentDice(prev => randomNumber
+      )
   }
 
   return (
     <DiceContainer>
-      <div className='dice'>
+      <div className='dice' onClick={() => generateRandomNumber(1, 7)}>
         <img src='/images/dice/dice_1.png' alt='dice 1' />
       </div>
       <p>Click on Dice to roll</p>
