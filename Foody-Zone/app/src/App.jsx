211f9b66f07c9402080d6ab9c4da85a06640245b
saikiran-modalavalasa <modalavalasasaikiran@gmail.com>
2024-02-28 23:15:43 +0530

    added backed and created new app

---
+++ Foody-Zone/app/src/App.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const App = () => {
+  return (
+  <MainContainer>
+    App
+  </MainContainer>);
+};
+
+export default App;
+
+const MainContainer = styled.div `
+
+
+
+`;
