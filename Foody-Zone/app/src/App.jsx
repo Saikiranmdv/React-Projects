ab78aa8141a71bb87571421207b6002cfb36a91e
saikiran-modalavalasa <modalavalasasaikiran@gmail.com>
2024-02-29 23:45:27 +0530

    Header part

--- Foody-Zone/app/src/App.jsx
+++ Foody-Zone/app/src/App.jsx
@@ -3,7 +3,16 @@ import styled from "styled-components";
 const App = () => {
   return (
   <MainContainer>
-    App
+    <TopContainer>
+      <div className="logo">
+        <img src="/logo.svg" alt="logo"/>
+      </div>
+      <div className="search">
+        <input
+          placeholder="Search Food..."
+        />
+      </div>
+    </TopContainer>
   </MainContainer>);
 };
 
@@ -11,6 +20,8 @@ export default App;
 
 const MainContainer = styled.div `
 
-
+background-color: #323334;
 
 `;
+
+const TopContainer = styled.section``;
