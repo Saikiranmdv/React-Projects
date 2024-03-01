0f5224bae9cd24c5975afb329a897b9f496c4ea0
saikiran-modalavalasa <modalavalasasaikiran@gmail.com>
2024-03-01 23:29:26 +0530

    Filter container and css changes

--- Foody-Zone/app/src/App.jsx
+++ Foody-Zone/app/src/App.jsx
@@ -13,6 +13,16 @@ const App = () => {
         />
       </div>
     </TopContainer>
+    <FilterContainer>
+      <Button>All</Button>
+      <Button>Breakfast</Button>
+      <Button>Lunch</Button>
+      <Button>Dinner</Button>
+    </FilterContainer>
+
+    <FoodCardsContainer>
+      <FoodCards></FoodCards>
+    </FoodCardsContainer>
   </MainContainer>);
 };
 
@@ -24,4 +34,46 @@ background-color: #323334;
 
 `;
 
-const TopContainer = styled.section``;
+const TopContainer = styled.section`
+
+min-height: 140px;
+display:flex;
+justify-content: space-between;
+padding: 16px;
+align-items: center;
+
+.search{
+  input{
+    background-color: transparent;
+    border: 1px solid red;
+    color: white;
+    border-radius: 5px;
+    height: 40px;
+    font-size: 16px;
+    padding: 0px 10px;
+  }
+}
+
+`;
+
+const FilterContainer = styled.section`
+display: flex;
+justify-content: center;
+gap: 12px;
+`;
+
+const Button = styled.button`
+  background-color: #ff4343;
+  border-radius: 5px;
+  padding: 6px 12px;
+  border:none;
+  color: white;
+`;
+
+const FoodCardsContainer = styled.section`
+height: calc(100vh - 170vh);
+background-image: url("/bg.png");
+background-size: cover;
+
+`;
+const FoodCards = styled.section``;
